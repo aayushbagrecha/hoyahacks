21d509ff6d3ac7e2f3c65c553c345ba18ae59bb0
kshitij101 <knarvekar31@gmail.com>
2025-01-25 16:11:00 -0500

    added mongo connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,18 @@
-from pymongo import MongoClient
-from app.config import MONGO_URI, DATABASE_NAME
+# from pymongo import MongoClient
+# from app.config import MONGO_URI, DATABASE_NAME
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from urllib.parse import quote_plus
 
-client = MongoClient(MONGO_URI)
-db = client[DATABASE_NAME]
+username = quote_plus('knarvekar31')
+password = quote_plus('Demo123456')
 
-# Collections
-user_collection = db["users"]
+uri = 'mongodb+srv://'+ username + ':' + password + '@' + 'cluster0.s5gaayu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
